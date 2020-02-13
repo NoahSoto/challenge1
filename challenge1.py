c7926d3e6993d057513c5080657e320603b3b548
Noah Soto <NoahSoto>
2020-02-13 17:19:44 -0500

    added comment

--- challenge1.py
+++ challenge1.py
@@ -1,9 +1,11 @@
 
+#python challenege number 1 of many...
+
 def addMatrix(matrixx,matrixxx):
     sumArray=[]
 
     for i in range(len(matrixx)):
-        sumArray.append([]) #adds a list after looping through each sub list.
+        sumArray.append([])         #adds a list after looping through each sub list.
         for j in range(len(matrixxx[i-1])):
            print((matrixx[i][j]+matrixxx[i][j]))        #adds and adds the sum.
            sumArray[i].append((matrixx[i][j]+matrixxx[i][j]))
