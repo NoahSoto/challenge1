ff4beeb31b04920d3ee83ab2ceb824607e90f98f
Noah Soto <NoahSoto>
2020-02-19 17:02:33 -0500

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+##README
+
+The first challenege talks about abouting matrixes.  I wrote something goofy but didnt save ugh.
+
+>using markdown is painfully easy.  _glad I spent the entire *3 minutes* _learning how to use it_
+
+'''
+i feel enlightened
+'''
--- challenge1.py
+++ challenge1.py
@@ -1,19 +1,39 @@
 
 #python challenege number 1 of many...
 
-def addMatrix(matrixx,matrixxx):
+def addMatrix(*matrixx):
+    listOfLists=[];
+    empty=[];
     sumArray=[]
+    arrayLength=len(matrixx)
 
-    for i in range(len(matrixx)):
-        sumArray.append([])         #adds a list after looping through each sub list.
-        for j in range(len(matrixxx[i-1])):
-           print((matrixx[i][j]+matrixxx[i][j]))        #adds and adds the sum.
-           sumArray[i].append((matrixx[i][j]+matrixxx[i][j]))
+    for matrix in matrixx:
+        listOfLists.append(matrixx)
+
+    token=0;
+    counter = 0
+    for i in arrayLength:
+        for j in range(len(listOfLists[i-1])):
+            sumArray[i]+=sumArray[i]
 
     return sumArray
+
+
 
 mat1=[[1,2],[1,2]]
 mat2=[[2,3],[4,5]]
 
 print(addMatrix(mat1,mat2))
+
+
+
+'''
+    for i in range(len(listOfLists)):
+        sumArray.append([])         #adds a list after looping through each sub list.
+        #sumArray.append(listOfLists[i])
+        for j in range(len(listOfLists[i-1])):
+            sumArray[i].append(listOfLists[i][j])
+  '''
+          # print((matrixx[i][j]+matrixxx[i][j]))        #adds and adds the sum.
+          # sumArray[i].append((matrixx[i][j]+matrixxx[i][j]))
 
